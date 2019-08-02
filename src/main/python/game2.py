a74c565cce382854663ed7b857d4e2b149b396c0
Hasari Tosun <hasari@gmail.coml>
2019-08-02 11:02:17 +0300

    summary

--- src/main/python/game2.py
+++ src/main/python/game2.py
@@ -6,20 +6,22 @@ def guess():
     for deneme in range(10):
         tahmin = int(input("Tahmin et:"))  # sonucu string. Sayiya cevirmek icin int() kullanmamiz lazim
         if tahmin == gizli:
-            print("Bravo... tahmin ettin, deneme sayisi:", deneme + 1)
+            print("Bravo! Dogru tahmin ettin. Deneme sayisi:", deneme + 1)
             break
 
         if tahmin < gizli:
             print("Daha buyuk...")
         else:
-            print ("Daha kucuk")
+            print ("Daha kucuk...")
 
         if deneme == 9:
             print("Daha fazla zeythin yagi ic...")
 
 
-play = input("Hosgeldiginiz numara tahmini oyununu oynamak istermisiniz?").lower()
+play = input("Hosgeldiginiz! Numara tahmini oyununu oynamak istermisiniz?").lower()
 
 while play != "h":
     guess()
     play = input("Bir daha oynamak istermisiniz? Hayir icin H yazin").lower()
+
+print("Oynadiginiz icin tesekkur ederim.")
---
+++ src/main/python/summary.py
@@ -0,0 +1,130 @@
+# Data types: data tipleri
+
+isim = "Eyub"  # String
+soyad = 'Karahanli'
+
+print(isim, "     -->", soyad, ":", 52)
+
+ad = "Ahmet"
+soyad = "KarahanliAAAKarabey"
+
+isim = ad + "AAA" + soyad
+print(isim)
+
+print(isim.lower())
+print(isim.split('AAA'))
+
+
+age = 12  # int sayi
+yas = '12'
+
+if age == yas:
+    print("Esittir")
+else:
+    print("Esit degildir.")
+
+yas = int(yas)
+if age == yas:
+    print("XXEsittir")
+else:
+    print("XXEsit degildir.")
+
+muz_fiyati = 12.0  # float ondalik sayilar.
+
+if age == muz_fiyati:
+    print("YYEsittir")
+else:
+    print("YYEsit degildir.")
+
+a = int(10 / 10)
+print(a)
+
+
+#list: liste
+
+isimler = ["Hasari", "Mehmet", "Ayse", "Ferhat"]
+ad = isimler[2]
+print(ad)
+#degerlerii degistirmek icin
+isimler[2] = "Fatma"
+print(isimler[2])
+#degerleri silmek icin
+
+del(isimler[2])
+print("Isimxx:",isimler[2])
+
+isimler.append("Ayse")
+
+print("ISIMLER = ", isimler)
+
+print(isimler)
+#her elemanina ulasmak icin (iterate)
+for isim in isimler:
+    print("Isim=", isim)
+
+
+for i in range(len(isimler)):
+    isimler[i] = isimler[i]+"X"
+
+print(isimler)
+
+
+sayilar = [7, 8, 9, 11, 6, 5]
+for i in range(len(sayilar)):
+    sayilar[i] = sayilar[i]*sayilar[i]
+
+print(sayilar)
+
+
+#algorithma fonksiyon tanimlama
+def kare_toplam(sayilar):
+    toplam = 0
+    for i in range(len(sayilar)):
+        toplam = toplam + sayilar[i]*sayilar[i]
+    return toplam
+
+
+#fonksiyonu calistirmak icin
+#sonuc = kare_toplam(sayilar)
+sonuc = kare_toplam([7, 8, 9, 11, 6, 5])
+print(sonuc)
+
+
+#en kucuk sayi
+def en_kucuk(sayilar):
+    kucuk = 999
+    for i in range(len(sayilar)):
+        if(sayilar[i] < kucuk):
+            kucuk = sayilar[i]
+    return kucuk
+
+sonuc = en_kucuk([7, 8, 9, 1, 11, 6, 5])
+print(sonuc)
+
+
+
+#dictionary :sozluk
+muhendisler = {'ahmet':12, 'mehmet':14, 'veli':49}
+print("ahmetin maasi",muhendisler['ahmet'])
+muhendisler['ahmet'] = 33
+print("ahmetin maasi",muhendisler['ahmet'])
+
+for isim in muhendisler:
+    print("maas",muhendisler[isim])
+
+for isim,maas in muhendisler.items():
+    print("isim=", isim, "MAAS=", maas)
+
+
+def max_salary(employee):
+    max = 0
+    zengin_isci = ""
+    for isim in employee:
+        if muhendisler[isim] > max:
+            max = muhendisler[isim]
+            zengin_isci = isim
+    return zengin_isci, max
+
+
+isim,en_buyuk_maas = max_salary(muhendisler)
+print("Zengin isim=", isim, "maas", en_buyuk_maas)

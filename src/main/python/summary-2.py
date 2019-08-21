9ca76f687acea9d657f45c8ae99a25d6e8bab758
Hasari Tosun <hasari@gmail.coml>
2019-08-21 14:48:06 +0300

    son ornek: degistir

--- src/main/python/summary-2.py
+++ src/main/python/summary-2.py
@@ -9,16 +9,22 @@ m = Student("Mehmet", "Ocak")
 
 # girdi parameters: inputs
 ogrenciler = [Student("Mehmet", "Agustos"), Student("Ahmet", "Ocak"), Student("Hemo", "Agustos")]
-cikti = [{"isim":"Mehmet", "ay":"Agustos"}, {"isim":"Ahmet", "ay":"Ocak"}, {"isim":"Hemo", "ay":"Agustos"}]
+cikti = [{"isim": "Mehmet", "ay": "Agustos"}, {"isim": "Ahmet", "ay": "Ocak"}, {"isim": "Hemo", "ay": "Agustos"}]
 
 sayilar = [3, 5, 6, 3, 6, 1, 2]
 
 
-
-
-
 def degistir(students):
-    pass
+    sonuc = []
+    for o in students:
+        ogrenci = {"isim": o.name, "ay": o.birthday}
+        sonuc.append(ogrenci)
+    return sonuc
+
+
+print("----------------------------")
+ogrencilerx = degistir(ogrenciler)
+print(ogrencilerx)
 
 
 def hesapla(sayilar):
@@ -33,6 +39,8 @@ def hesapla(sayilar):
 
 hesaplamalar = hesapla(sayilar)
 print(hesaplamalar)
+
+
 def birthdays(ogrenciler):
     sonuc = {}
     for ogrenci in ogrenciler:

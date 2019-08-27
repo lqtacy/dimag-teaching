48fcf0d90632130f86b81e8c34e7f500d5c8afab
Hasari Tosun <hasari@gmail.coml>
2019-08-27 11:23:59 +0300

    searcher

--- src/main/python/lambda.py
+++ src/main/python/lambda.py
@@ -41,10 +41,12 @@ f = f_c(3)
 
 print(f(5))
 
+
 def fx(x):
     return 3 * x + 1
 
-#anonim fonksiyon denilir
+
+# anonim fonksiyon denilir
 f_x = lambda x: 3 * x + 1
 
 
@@ -54,7 +56,6 @@ def filter(numbers, fn):
         if fn(x):
             sonuc.append(x)
     return sonuc
-
 
 
 numbers = [0, 9, 11, 12, -1, -2, 10, 3, 5, 8, 10]
@@ -67,37 +68,42 @@ print(sonuclar)
 sonuclar = filter(numbers, lambda x: x % 2 == 0)
 print(sonuclar)
 
-
 sonuclar = filter(numbers, lambda x: x > 10)
 print(sonuclar)
 
 isimler = ["Hasari", "Mehmet", "Elif", "Fatma", "Murat", "Haydar"]
-sonuclar  = filter(isimler, lambda x: len(x) > 4)
+sonuclar = filter(isimler, lambda x: len(x) > 4)
 print(sonuclar)
 
-sonuclar  = filter(isimler, lambda x: x[0] == 'H')
+sonuclar = filter(isimler, lambda x: x[0] == 'H')
 print(sonuclar)
 
-sonuclar  = filter(isimler, lambda x: x[len(x)-1] == 'f' or x[len(x)-1] == 'F')
+sonuclar = filter(isimler, lambda x: x[len(x) - 1] == 'f' or x[len(x) - 1] == 'F')
 print(sonuclar)
 
-sonuclar  = filter(isimler, lambda x: x[-1] == 'f' or x[-1] == 'F')
+sonuclar = filter(isimler, lambda x: x[-1] == 'f' or x[-1] == 'F')
 print(sonuclar)
 
-#Odev 1: H is ile baslayip r ile biten isimleri listelesini
 
-#Odev 2: Yeni bir filter fonksiyonu asagidaki ogrenci (student listesi icin yazin)
+# Odev 1: H is ile baslayip r ile biten isimleri listelesini
+
+# Odev 2: Yeni bir filter fonksiyonu asagidaki ogrenci (student listesi icin yazin)
 class Student:
     def __init__(self, isim, dogum_ayi):
         self.name = isim
         self.birthday = dogum_ayi
 
+    def __str__(self):
+        return "name:" + self.name + ", birthday:" + self.birthday
+
+
 def student_filter(students, fn):
-    pass
+    return filter(students, fn)
 
 
 # girdi parameters: inputs
 ogrenciler = [Student("Mehmet", "Agustos"), Student("Ahmet", "Ocak"), Student("Hemo", "Agustos")]
-#yaz aylarinda dogan ogrencileri filtrelesin.
-sonuclar = student_filter(ogrenciler, lambda x: x)
-
+# yaz aylarinda dogan ogrencileri filtrelesin.
+sonuclar = student_filter(ogrenciler, lambda x: x.birthday == 'Ocak')
+for s in sonuclar:
+    print(s)

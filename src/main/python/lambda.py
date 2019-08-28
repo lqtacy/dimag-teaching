20fe2affa25c429d4a37bead95f2ae37371fc134
Hasari Tosun <hasari@gmail.coml>
2019-08-28 15:49:31 +0300

    list comprehensions

--- src/main/python/lambda.py
+++ src/main/python/lambda.py
@@ -3,7 +3,7 @@
 
 # 1 - write filter function with lambda parameter x < 0, x>0, x % 2==0
 # 2 - Homework: studends born in summer.
-
+from student import Student
 
 def f3(x):
     return 3 * x
@@ -50,60 +50,112 @@ def fx(x):
 f_x = lambda x: 3 * x + 1
 
 
-def filter(numbers, fn):
+def filter(fn, elements):
     sonuc = []
-    for x in numbers:
-        if fn(x):
-            sonuc.append(x)
+    for x in elements:
+        if fn(x): #eger dogru ise (test et)
+            sonuc.append(x)  #ekle
     return sonuc
 
 
 numbers = [0, 9, 11, 12, -1, -2, 10, 3, 5, 8, 10]
-sonuclar = filter(numbers, lambda x: x < 0)
+sonuclar = filter(lambda x: x < 0, numbers)
 print(sonuclar)
 
-sonuclar = filter(numbers, lambda x: x >= 0)
+sonuclar = filter(lambda x: x >= 0, numbers)
 print(sonuclar)
 
-sonuclar = filter(numbers, lambda x: x % 2 == 0)
+sonuclar = filter(lambda x: x % 2 == 0, numbers)
 print(sonuclar)
 
-sonuclar = filter(numbers, lambda x: x > 10)
+sonuclar = filter(lambda x: x > 10, numbers)
 print(sonuclar)
 
 isimler = ["Hasari", "Mehmet", "Elif", "Fatma", "Murat", "Haydar"]
-sonuclar = filter(isimler, lambda x: len(x) > 4)
+sonuclar = filter(lambda x: len(x) > 4, isimler)
 print(sonuclar)
 
-sonuclar = filter(isimler, lambda x: x[0] == 'H')
+sonuclar = filter(lambda x: x[0] == 'H', isimler)
 print(sonuclar)
 
-sonuclar = filter(isimler, lambda x: x[len(x) - 1] == 'f' or x[len(x) - 1] == 'F')
+sonuclar = filter(lambda x: x[len(x) - 1] == 'f' or x[len(x) - 1] == 'F', isimler)
 print(sonuclar)
 
-sonuclar = filter(isimler, lambda x: x[-1] == 'f' or x[-1] == 'F')
+sonuclar = filter(lambda x: x[-1] == 'f' or x[-1] == 'F', isimler)
 print(sonuclar)
 
 
 # Odev 1: H is ile baslayip r ile biten isimleri listelesini
 
 # Odev 2: Yeni bir filter fonksiyonu asagidaki ogrenci (student listesi icin yazin)
-class Student:
-    def __init__(self, isim, dogum_ayi):
-        self.name = isim
-        self.birthday = dogum_ayi
-
-    def __str__(self):
-        return "name:" + self.name + ", birthday:" + self.birthday
-
-
-def student_filter(students, fn):
-    return filter(students, fn)
 
 
 # girdi parameters: inputs
-ogrenciler = [Student("Mehmet", "Agustos"), Student("Ahmet", "Ocak"), Student("Hemo", "Agustos")]
+ogrenciler = [Student("Mehmet", "Haziran"), Student("Ahmet", "Ocak"), Student("Hemo", "Agustos")]
 # yaz aylarinda dogan ogrencileri filtrelesin.
-sonuclar = student_filter(ogrenciler, lambda x: x.birthday == 'Ocak')
+sonuclar = filter(lambda x: x.birthday == 'Haziran' or x.birthday == 'Temmuz' or x.birthday == 'Agustos', ogrenciler)
 for s in sonuclar:
     print(s)
+
+
+sonuclar = filter (lambda x: x < 0, [4, -5, 6, 8, 9,-1])
+print(sonuclar)
+
+
+kelime = "kayak"
+
+#tersini verecek
+def reverse(word):
+    sonuc = ''
+    for i in range(len(word)-1, -1, -1):
+        sonuc = sonuc + word[i]
+    return sonuc
+
+
+
+#palindrome
+# def is_palindrome(word):
+#     if word == reverse(word):
+#         return True
+#     else:
+#         return False
+
+
+
+#palindrome
+def is_palindrome(word):
+    return word == reverse(word)
+
+
+result = is_palindrome("haziran")
+print(result)
+result = is_palindrome("kayak")
+print(result)
+
+def f(x, y):
+    return x+y
+
+fxy = lambda x, y: x+y
+
+print(fxy(7,8))
+
+numbers = [6, 7, 9,2, 1, 11]
+
+def square(numbers):
+    sonuclar = []
+    for x in numbers:
+        sonuclar.append(x*x)
+    return sonuclar
+
+
+sonuc = [x*x for x in numbers] #list Comprehensions
+
+print(sonuc)
+
+
+sonuc = [x.birthday for x in ogrenciler] #list Comprehensions
+print(sonuc)
+
+kelimeler = ["haziran", "kayak", "mom", "racecar", "temmuz"]
+sonuc = [is_palindrome(x) for x in kelimeler] #list Comprehensions
+print(sonuc)
